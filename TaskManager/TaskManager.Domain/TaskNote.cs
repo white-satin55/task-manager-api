deac19b8ff7ec55804f516eccd9c3e57d0c954c9
Egor Sazanov <egorsazanov97@gmail.com>
2023-08-07 15:17:46 +0300

    Project inizialized

---
+++ TaskManager/TaskManager.Domain/TaskNote.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace TaskManager.Domain
+{
+    public class TaskNote
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? Deadline { get; set; }
+
+    }
+}
