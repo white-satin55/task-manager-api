89f30a8643a5351403bd0ba014dc5161ff8680fe
Egor Sazanov <egorsazanov97@gmail.com>
2023-08-07 15:32:48 +0300

    Added repository

---
+++ TaskManager/TaskManager.Application/ITaskNoteRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.Domain;
+
+namespace TaskManager.Application
+{
+    public interface ITaskNoteRepository
+    {
+        ICollection<TaskNote> GetAllTasks();
+        TaskNote GetTask(int id);
+        void CreateTask(TaskNote note);
+        void UpdateTask(TaskNote note);
+        TaskNote DeleteTask(int id);
+    }
+}
